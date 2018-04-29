5023b60f218df1da5d2193d91adac9a6dd93c9eb
ponkore <ponkore@gmail.com>
2018-04-30 00:57:18 +0900

    test.sh removed, *.ts moved

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -x
-BASE_URL='https://ensq626dbg.execute-api.us-east-1.amazonaws.com/dev'
-
-curl -X PUT "${BASE_URL}/fugapiyo"
-
-  # accessKeyId: 'AKIAIXKZ55RIXWFN2OAQ',
-  # secretAccessKey: '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'
-# mkdir /files

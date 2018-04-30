62ee5d44f4c576a1655d0f9a17ea5552eaa35d10
ponkore <ponkore@gmail.com>
2018-04-30 18:09:53 +0900

    move configure code to test.ts

--- handler.ts
+++ handler.ts
@@ -1,37 +1,5 @@
 import AWS = require('aws-sdk');
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
-import { readFileSync, existsSync } from 'fs';
-
-const HOME = process.env['HOME'];
-const credentials = `${HOME}/.aws/credentials`;
-
-let ACCESS_KEY_ID = '';
-let SECRET_ACCESS_KEY = '';
-
-if (existsSync(credentials)) {
-  let contents = readFileSync(credentials)
-    .toString()
-    .split('\n');
-  for (let i = 0; i < contents.length; i++) {
-    let s = contents[i];
-    let re1 = new RegExp('^aws_access_key_id = ([A-Za-z0-9]+)').exec(s);
-    if (re1) {
-      ACCESS_KEY_ID = re1[1];
-    }
-    let re2 = new RegExp('^aws_secret_access_key = ([A-Za-z0-9]+)').exec(s);
-    if (re2) {
-      SECRET_ACCESS_KEY = re2[1];
-    }
-  }
-} else {
-  ACCESS_KEY_ID = process.env['ACCESS_KEY_ID'];
-  SECRET_ACCESS_KEY = process.env['SECRET_ACCESS_KEY'];
-}
-
-AWS.config.update({
-  accessKeyId: ACCESS_KEY_ID,
-  secretAccessKey: SECRET_ACCESS_KEY
-});
 
 const S3 = new AWS.S3({
   region: 'us-east-1',
